a77d9c20490977f2f2bc37927107189247b82f92
Aravind G <aravind.gopal.1998@gmail.com>
2022-04-10 15:29:19 +0530

    added function to test any CF model

---
+++ models.py
@@ -0,0 +1,125 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+
+class CF(Module):
+    """Simple Collaborative Filtering"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(CF, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        dot = (u * i).sum(1)
+        return torch.sigmoid(dot)
+
+class CFD(Module):
+    """Simple Collaborative Filtering with dropout"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(CFD, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+        self.drop_u = nn.Dropout(p = 0.3)
+        self.drop_i = nn.Dropout(p = 0.3)
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        u = self.drop_u(u)
+        i = self.drop_i(i)
+        dot = (u * i).sum(1)
+        return torch.sigmoid(dot)
+
+class GMF(Module):
+    """General Matrix Factorization with 1 hidden layer"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(GMF, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+        self.h = nn.Linear(n_factors, 1)
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        dot = (u * i)
+        x = self.h(dot).squeeze()
+        return torch.sigmoid(x)
+
+class GMFD(Module):
+    """General Matrix Factorization with 1 hidden layer and dropout"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(GMFD, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+        self.h = nn.Linear(n_factors, 1)
+        self.drop_u = nn.Dropout(p = 0.2)
+        self.drop_i = nn.Dropout(p = 0.2)
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        u = self.drop_u(u)
+        i = self.drop_i(i)
+        dot = (u * i)
+        x = self.h(dot).squeeze()
+        return torch.sigmoid(x)
+
+# GMF with user-item bias and dropout
+class GMFB(Module):
+    """General Matrix Factorization with user-item bias, 1 hidden layer and dropout"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(GMFB, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+        self.h = nn.Linear(n_factors * 3, 1)
+        self.drop_u = nn.Dropout(p = 0.2)
+        self.drop_i = nn.Dropout(p = 0.2)
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        u = self.drop_u(u)
+        i = self.drop_i(i)
+        dot = (u * i)
+        feat = torch.concat([dot, u, i], 1)
+        x = self.h(feat).squeeze()
+        return torch.sigmoid(x)
+
+class MLP(Module):
+    """Multi-layer Perceptron without hadamard product"""
+    def __init__(self, n_users, n_items, n_factors):
+        super(MLP, self).__init__()
+        self.user_emb = nn.Embedding(n_users, n_factors)
+        self.item_emb = nn.Embedding(n_items, n_factors)
+        self.h = nn.Linear(n_factors * 2, n_factors * 2)
+        self.o = nn.Linear(n_factors * 2, 1)
+        self.drop_u = nn.Dropout(p = 0.2)
+        self.drop_i = nn.Dropout(p = 0.2)
+        self.drop_x = nn.Dropout(p = 0.2)
+        self.tanh = nn.Tanh()
+
+    def forward(self, user, item):
+        u = self.user_emb(user)
+        i = self.item_emb(item)
+        u = self.drop_u(u)
+        i = self.drop_i(i)
+        x = torch.concat([u, i], 1)
+        # x = self.tanh(x)
+        x = self.h(x)
+        # x = self.drop_x(x)
+        x = F.relu(x) # 1 (both relu and sigmoid seem to work fine here)
+        # x = torch.sigmoid(x) # 2
+        # x = self.tanh(x) # 3
+        x = self.o(x)
+        return torch.sigmoid(x.squeeze())
+
+def get_accuracy(y_hat, y):
+    y = y.clone().int()
+    y_hat = (y_hat.clone() > 0.5).int()
+    accuracy = (y == y_hat).sum() / len(y)
+    return accuracy.item()
